1fee857e65acf6bbbf4fa671bce76d73344d0df8
shreyash1996 <shreyashdeulkar01@gmail.com>
2025-02-14 20:30:09 +0530

    sadfd

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # apnacollege-demo
 this is my first my repro
 author-shreyash deulkar
-hello i am shareyash deulkar
+hello i am shareyash deulkarggg
---
+++ projects/chatbot/simple-bot.py
@@ -0,0 +1,15 @@
+import asyncio
+from wechaty import Wechaty, Message
+
+async def on_message(msg: Message):
+    if msg.text() == 'ding':
+        await msg.say('dong')
+    
+async def main():
+    bot = Wechaty()
+    bot.on('message', on_message)
+    await bot.start()
+
+asyncio.run(main())
+
+hello i am adding python complile time code and praciting git and git hub
